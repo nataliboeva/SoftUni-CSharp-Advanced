620720fe787f9d580e54f6aef6d6f4bacce17385
Natali Boeva <121763213+nataliboeva@users.noreply.github.com>
2024-05-30 21:52:46 +0300

    Add files via upload

---
+++ 05. Sets and Dictionaries Advanced/01.CountSameValuesInArray.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.CountSameValuesInArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            Dictionary<double, int> SameValuesCount = new ();
+
+            foreach (double number in numbers)
+            {
+                if (SameValuesCount.ContainsKey(number))
+                {
+                    SameValuesCount[number]++;
+                }
+                else
+                {
+                    SameValuesCount.Add(number, 1);
+                }
+            }
+
+            foreach(var kvp in SameValuesCount)
+            {
+                Console.WriteLine($"{kvp.Key} - {kvp.Value} times");
+            }
+        }
+    }
+}
